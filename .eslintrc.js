dfb49246eed3cf8dbd02e26ed07f2626afda1876
José Cabarcas <jocabarcasg@hotmail.com>
2024-04-10 14:36:54 -0500

    chore: :art: Lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,103 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  ignorePatterns: [
+    '**/*/*.js',
+    '*.js',
+    '*.svg',
+    '*.json',
+    '*.png',
+    '**/node_modules/**',
+    'package.json',
+    'package-lock.json',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    tsconfigRootDir: './',
+  },
+  plugins: [
+    'react',
+    'react-native',
+    'react-hooks',
+    '@typescript-eslint',
+    'import',
+    'prettier',
+  ],
+  rules: {
+    'max-len': ['error', 150],
+    // 'sort-imports': [
+    //   'error',
+    //   {
+    //     ignoreCase: false,
+    //     ignoreDeclarationSort: false,
+    //     ignoreMemberSort: false,
+    //     memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+    //     allowSeparatedGroups: false,
+    //   },
+    // ],
+    // 'array-bracket-spacing': 1,
+    // 'object-curly-spacing': 1,
+    '@typescript-eslint/ban-ts-comment': 2,
+    '@typescript-eslint/no-explicit-any': 1,
+    'react-native/no-unused-styles': 2,
+    'react-native/no-inline-styles': 1,
+    '@typescript-eslint/no-empty-interface': 1,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 0,
+    'prefer-destructuring': 2,
+    'no-nested-ternary': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/no-unused-modules': 'error',
+    'arrow-spacing': 'error',
+    'import/order': [
+      'warn',
+      {
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
